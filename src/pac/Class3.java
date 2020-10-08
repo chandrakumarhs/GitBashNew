0ce6eec3d9edafdfcbfba42bd93d66fd610cfec9
chandrakumarhs <hschandra655@gmail.com>
2020-10-08 13:23:37 +0530

    Revert "Class 3 modified and Class 5 added"
    This reverts commit 624a6ec103c610b071594d2912c34998038a81fa.

--- src/pac/Class3.java
+++ src/pac/Class3.java
@@ -3,7 +3,6 @@ package pac;
 public class Class3 {
 	public static void main(String[] args) {
 		System.out.println("Class 3 added");
-		System.out.println("Class 3 modified");
 	}
 
 }
--- src/pac/Class5.java
+++
@@ -1,9 +0,0 @@
-package pac;
-
-public class Class5 {
-
-	public static void main(String[] args) {
-		System.out.println("Class 5 created");
-	}
-
-}
