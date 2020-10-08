ae55605cd6a4814a15bf70d669002a500b3167d3
chandrakumarhs <hschandra655@gmail.com>
2020-10-08 13:20:13 +0530

    Class 3 and Class 4 added

---
+++ src/pac/Class3.java
@@ -0,0 +1,8 @@
+package pac;
+
+public class Class3 {
+	public static void main(String[] args) {
+		System.out.println("Class 3 added");
+	}
+
+}
---
+++ src/pac/Class4.java
@@ -0,0 +1,10 @@
+package pac;
+
+public class Class4 {
+
+	public static void main(String[] args) {
+		System.out.println("Class 4 is added");
+
+	}
+
+}
