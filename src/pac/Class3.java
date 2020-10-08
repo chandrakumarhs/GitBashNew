624a6ec103c610b071594d2912c34998038a81fa
chandrakumarhs <hschandra655@gmail.com>
2020-10-08 13:22:20 +0530

    Class 3 modified and Class 5 added

--- src/pac/Class3.java
+++ src/pac/Class3.java
@@ -3,6 +3,7 @@ package pac;
 public class Class3 {
 	public static void main(String[] args) {
 		System.out.println("Class 3 added");
+		System.out.println("Class 3 modified");
 	}
 
 }
---
+++ src/pac/Class5.java
@@ -0,0 +1,9 @@
+package pac;
+
+public class Class5 {
+
+	public static void main(String[] args) {
+		System.out.println("Class 5 created");
+	}
+
+}
